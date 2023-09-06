3a65efb6ed5fe6d9e192ee615030aa52de87a133
StigmaQueen <171g0218@rcarbonifera.tecnm.mx>
2023-09-05 18:50:23 -0600

    Lección 5

---
+++ IntProyectoSwRazorPages/Pages/Lección5.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace IntProyectoSwRazorPages.Pages
+{
+    public class Lección5Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
