ce93142028ac69695c489ad81f61e8a9d0f54697
StigmaQueen <171g0218@rcarbonifera.tecnm.mx>
2023-09-11 18:43:59 -0600

    Lección 9

---
+++ IntProyectoSwRazorPages/Pages/Lección8.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace IntProyectoSwRazorPages.Pages
+{
+    public class Lección8Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ IntProyectoSwRazorPages/wwwroot/scripts/alumnos.js
@@ -0,0 +1,83 @@
+const lista = [
+    {
+        "NumControl": "191G0227",
+        "Nombre": "ANDRADE SILOS GUILLERMO SAUL"
+    },
+    {
+        "NumControl": "191G0605",
+        "Nombre": "BAUTISTA CERVANTES JESUS EDUARDO"
+    },
+    {
+        "NumControl": "191G0706",
+        "Nombre": "BORJON CASTILLO XIMENA"
+    },
+    {
+        "NumControl": "171G0218",
+        "Nombre": "BRIONES ZUÑIGA ILSE CECILIA"
+    },
+    {
+        "NumControl": "191G0235",
+        "Nombre": "CUAUTLE ZACAMITZIN CARLOS"
+    },
+    {
+        "NumControl": "191G0239",
+        "Nombre": "DELGADO VILLAFAN MIGUEL ANGEL"
+    },
+    {
+        "NumControl": "191G0243",
+        "Nombre": "FERNANDEZ CUELLAR ALBERTO"
+    },
+    {
+        "NumControl": "191G0247",
+        "Nombre": "GALLEGOS HERNANDEZ CARLOS ABRAHAM"
+    },
+    {
+        "NumControl": "191G0249",
+        "Nombre": "GONZALEZ LEOS KARINA"
+    },
+    {
+        "NumControl": "191G0260",
+        "Nombre": "IBARRA HERNANDEZ ESTEFANIA ELIZABETH"
+    },
+    {
+        "NumControl": "191G0262",
+        "Nombre": "JUAREZ HERNANDEZ MIGUEL ANGEL"
+    },
+    {
+        "NumControl": "191G0263",
+        "Nombre": "LOPEZ DE LA CRUZ DAVID ARMANDO"
+    },
+    {
+        "NumControl": "191G0265",
+        "Nombre": "MARTINEZ DE LEON ADRIANA MONSERRAT"
+    },
+    {
+        "NumControl": "191G0267",
+        "Nombre": "MARTINEZ PALOS FELIX DANIEL"
+    },
+    {
+        "NumControl": "191G0281",
+        "Nombre": "RAMIREZ GONZALEZ CESAR ALEJANDRO"
+    },
+    {
+        "NumControl": "191G0289",
+        "Nombre": "RODRIGUEZ WONG ROBERTO"
+    },
+    {
+        "NumControl": "191G0129",
+        "Nombre": "RUIZ ALMAGUER BELEN"
+    },
+    {
+        "NumControl": "191G0291",
+        "Nombre": "SAN MIGUEL POMPA ENRIQUE ALONSO"
+    },
+    {
+        "NumControl": "181G0533",
+        "Nombre": "TORRES MARTINEZ ABRAHAM ANTONIO"
+    },
+    {
+        "NumControl": "191G0710",
+        "Nombre": "VALDEZ BELTRAN CARLOS ALBERTO"
+    }
+];
+
