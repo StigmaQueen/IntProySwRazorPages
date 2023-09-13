e93e71e5ea38eef3808cc7abe6df1487c5f6910d
StigmaQueen <171g0218@rcarbonifera.tecnm.mx>
2023-09-12 18:52:34 -0600

    Leccón 9

---
+++ IntProyectoSwRazorPages/Pages/Lección9.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace IntProyectoSwRazorPages.Pages
+{
+    public class Lección9Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
