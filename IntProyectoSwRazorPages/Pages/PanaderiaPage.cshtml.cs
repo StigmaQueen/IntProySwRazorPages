71812dab17fe0a9d0a072dcef0620a679cc33dd7
StigmaQueen <171g0218@rcarbonifera.tecnm.mx>
2023-09-26 18:47:35 -0600

    Panaderia Cinnamoroll

---
+++ IntProyectoSwRazorPages/Pages/PanaderiaPage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace IntProyectoSwRazorPages.Pages
+{
+    public class PanaderiaPageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ IntProyectoSwRazorPages/wwwroot/scripts/PanaderiaCinnamoroll.js
@@ -0,0 +1,19 @@
+﻿//https://integracion.itesrc.net/api/panes
+//las operaciones tienen que ser asyncronas-asyncronia es desconocer el tiempo que le va a tomar a la operación realizar una tarea-//
+//hacer un request
+
+var listaPanecillos;
+
+async function descargarProcuctos()
+{
+    let response = await fetch("https://integracion.itesrc.net/api/panes");
+
+    if (response.ok)
+    {
+        ///Recuperar los datos
+        listaPanecillos= await response.json();
+        console.log(content);
+    }
+}
+
+descargarProcuctos();
