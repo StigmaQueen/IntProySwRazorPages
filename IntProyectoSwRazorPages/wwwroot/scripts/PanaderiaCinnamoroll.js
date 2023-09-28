9037078b880dbb9456032ba23e22e5eb897bb2fa
StigmaQueen <171g0218@rcarbonifera.tecnm.mx>
2023-09-27 18:50:16 -0600

    cambios

--- IntProyectoSwRazorPages/wwwroot/scripts/PanaderiaCinnamoroll.js
+++ IntProyectoSwRazorPages/wwwroot/scripts/PanaderiaCinnamoroll.js
@@ -3,17 +3,38 @@
 //hacer un request
 
 var listaPanecillos;
+const botonera = document.querySelector(".panes div");
 
-async function descargarProcuctos()
-{
+async function descargarProcuctos() {
     let response = await fetch("https://integracion.itesrc.net/api/panes");
 
-    if (response.ok)
-    {
+    if (response.ok) {
         ///Recuperar los datos
-        listaPanecillos= await response.json();
+        listaPanecillos = await response.json();
         console.log(content);
     }
 }
+
+function Botoneria()
+{
+
+    listaPanecillos.forEach(x => {
+        let btn = document.createElement("BUTTON");
+        btn.dataset.id = x.id;
+        let img = document.createElement("IMG");
+        img.src = x.imagen;
+        let txt = document.createTextNode("$" + x.precio.toFixed(2));
+        btn.appendChild(img, txt);
+        botonera.append(btn);
+    });
+
+}
+
+botonera.addEventListener("clik", function (e) {
+    if (e.target.tagName == "BUTTON" | e.target.tagName == "IMG") {
+        alert(e.target.tagName.id);
+       
+    }
+});
 
 descargarProcuctos();
